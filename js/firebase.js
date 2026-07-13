637124f4afa369db067cd9207972aba41df59d10
agj235 <agj235@users.noreply.github.com>
2026-07-12 21:04:39 -0400

    Align Firebase config with project settings

--- js/firebase.js
+++ js/firebase.js
@@ -3,7 +3,7 @@
     apiKey: 'AIzaSyARCnBdNuUfPNtZ_otEn-MWpktzShD_NWE',
     authDomain: 'rambofitness-81303.firebaseapp.com',
     projectId: 'rambofitness-81303',
-    storageBucket: 'rambofitness-81303.appspot.com',
+    storageBucket: 'rambofitness-81303.firebasestorage.app',
     messagingSenderId: '3674488945',
     appId: '1:3674488945:web:58e907ce543b1b87bbdbd0'
   };
