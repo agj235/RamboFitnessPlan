7b656101ee97859485a1f3ea2afa4ddfab22e2ca
Angel Jimenez <39224072+agj235@users.noreply.github.com>
2026-07-16 23:31:53 -0400

    Delete state.js

--- state.js
+++
@@ -1,8 +0,0 @@
-(function () {
-  window.RamboAppState = window.RamboAppState || {
-    currentUser: null,
-    currentProgram: localStorage.getItem('currentProgram') || '5day',
-    currentWorkouts: [],
-    currentDay: 1
-  };
-})();
